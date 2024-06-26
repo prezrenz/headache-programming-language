7448ba103d0b840007140028e78282ff857d65a2
prezrenz <perezrenjie20@gmail.com>
2024-06-26 10:45:26 +0800

    implement repl

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 
+/* LEXER */
+
+
+
 int main(int argc, char** argv)
 {
     FILE* program;
 
-    if(argc != 2) {
-        fprintf(stderr, "Usage: headache <program>");
+    if(argc == 1) {
+        char input[2048];
+
+        puts("Headache REPL");
+        puts("Press Ctrl+C to Exit\n");
+
+        while (1) {
+            fputs("headache> ", stdout);
+            fgets(input, 2048, stdin);
+            printf("Input was: %s", input);
+        }
+    }
+    else if(argc > 2)
+    {
+        fprintf(stderr, "Usage: headache (program)\n\tOmit program to enter REPL");
         return 1;
     }
     else if((program = fopen(argv[1], "r")) == NULL) {
